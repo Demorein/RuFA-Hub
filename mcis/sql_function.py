389743f6d81a73f4b2e35e4e0183c9ad7c5b71ff
Demorein <demorein6@gmail.com>
2025-02-10 22:26:22 +0000

    delete coments

--- mcis/sql_function.py
+++ mcis/sql_function.py
@@ -4,12 +4,6 @@ from os import path
 class sql_func:
     def __init__(self, db_name):
         
-        '''
-        Функция котрая проверяет наличие базы данный по названием db_name.\n
-        Если такой базы нет, то создаёт новую с название6 bd_name\n
-        Создаёт таблицу hosts с столбцами id, ip, port
-        '''
-        
         self.db_name = db_name
         
         if not path.exists(self.db_name):
@@ -29,22 +23,15 @@ class sql_func:
 
             
             
-    def insert_host(self, ip, port):
-        
-        '''
-        Проверяет наличие записи с ip port в таблице\n
-        Если записей нет, то добавляет ip port в таблицу
-        '''
-        
+    def insert_host(self, ip, port):  
         
         conn = sqlite3.connect(self.db_name)
         cursor = conn.cursor()
 
-        # Проверяем, есть ли уже такая запись
         cursor.execute("SELECT COUNT(*) FROM hosts WHERE ip = ? AND port = ?", (ip, port))
         result = cursor.fetchone()
 
-        if result[0] == 0:  # Если записей с таким IP и портом нет
+        if result[0] == 0:
             cursor.execute("INSERT INTO hosts (ip, port) VALUES (?, ?)", (ip, port))
             conn.commit()
             conn.close()
@@ -54,14 +41,7 @@ class sql_func:
             return False
 
 
-
-
-
     def delete_host(self, ip, port):
-        
-        '''
-        Удаляет запись с ip port
-        '''
         
         conn = sqlite3.connect(self.db_name)
         cursor = conn.cursor()
@@ -80,7 +60,7 @@ class sql_func:
 
 
     def delete_all_hosts(self):
-        """Удаляет все записи из таблицы"""
+
         conn = sqlite3.connect(self.db_name)
         cursor = conn.cursor()
 
