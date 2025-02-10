901af80ba17ebe0e82af7144e42eae8bcbc57243
EvgenyGr <140063448+Demorein@users.noreply.github.com>
2025-02-11 00:23:05 +0500

    mcis_srv/cli and sql_func

--- mcis/mcis_server.py
+++ mcis/mcis_server.py
@@ -1,1 +1,13 @@
 #MCIS Server
+
+import socket
+import config
+
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.bind((f'{config.server_host[0]}', {config.server_host[1]}))
+
+while True:
+    message, client_address = server_socket.recvfrom(1024)
+    print(message.decode())
+    server_socket.sendto(b"OK", client_address)
