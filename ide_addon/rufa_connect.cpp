fd831e6527ff445e80080c914bd01c7ad9df189d
EvgenyGr <140063448+Demorein@users.noreply.github.com>
2025-02-11 01:40:08 +0500

    Update rufa_connect.cpp

--- ide_addon/rufa_connect.cpp
+++ ide_addon/rufa_connect.cpp
@@ -3,22 +3,18 @@
 
 #include "RuFAConnect.h"
 
-// Конструктор
 RuFAConnect::RuFAConnect(HardwareSerial &serial) {
   _serial = &serial;
 }
 
-// Инициализация порта
 void RuFAConnect::begin(long baudRate) {
   _serial->begin(baudRate);
 }
 
-// Отправка данных
 void RuFAConnect::sendData(const char* data) {
-  _serial->println(data);  // Отправляем строку через последовательный порт
+  _serial->println(data);
 }
 
-// Получение данных (если нужно)
 String RuFAConnect::receiveData() {
   String data = "";
   while (_serial->available()) {
