12097d0ce6ccee2ed763e579f6f03c98508c29d7
EvgenyGr <140063448+Demorein@users.noreply.github.com>
2025-02-11 01:38:17 +0500

    Update rufa_connect.hh

--- ide_addon/rufa_connect.hh
+++ ide_addon/rufa_connect.hh
@@ -5,20 +5,16 @@
 
 class RuFAConnect {
   public:
-    // Конструктор, инициализация с указанием последовательного порта
     RuFAConnect(HardwareSerial &serial);
     
-    // Инициализация библиотеки (например, установка скорости порта)
     void begin(long baudRate);
     
-    // Отправка данных на RuFA Connect
     void sendData(const char* data);
     
-    // Получение данных от RuFA Connect (если требуется)
     String receiveData();
     
   private:
-    HardwareSerial *_serial;  // Указатель на объект последовательного порта
+    HardwareSerial *_serial; 
 };
 
 #endif
