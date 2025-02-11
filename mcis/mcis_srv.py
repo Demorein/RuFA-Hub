a7bc81d90122e31cb70abf638e830a4e97f24c1e
Demorein <demorein6@gmail.com>
2025-02-11 22:15:19 +0000

    Добавлен класс mcis_srv

--- mcis/mcis_srv.py
+++ mcis/mcis_srv.py
@@ -1,33 +1,43 @@
-#MCIS Server
-
 import secrets
 import socket
 import config
 import sql_function
 import func
 import json
+import queue
 
-db = sql_function.sql_func(config.database_name)
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_socket.bind((f'{config.server_host[0]}', config.server_host[1]))
+class mcis_srv:
+    def __init__(self, data_queue):
+        self.db = sql_function.sql_func(config.database_name)
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.server_socket.bind((f'{config.server_host[0]}', config.server_host[1]))
+        self.data_queue = data_queue  # Очередь для передачи данных в основной поток
 
-while True:
-    message, client_address = server_socket.recvfrom(1024)
-    print(message.decode())
+    def start_mcis_srv(self):
+        while True:
+            message, client_address = self.server_socket.recvfrom(1024)
+            print(message.decode())
 
-    if message.decode() == "I Here!": 
-        if db.insert_host(client_address[0], 990, secrets.token_hex(16)):
-            print(db.show_all_hosts())
-            server_socket.sendto(b"OK", client_address)
-            func._elogs(f"Пользователь ip {client_address} зарегистрирован", ecode = 200)
+            if message.decode() == "I Here!":
+                if self.db.insert_host(client_address[0], 990, secrets.token_hex(16)):
+                    print(self.db.show_all_hosts())
+                    self.server_socket.sendto(b"OK", client_address)
+                    func._elogs(f"Пользователь ip {client_address} зарегистрирован", ecode = 200)
+                else:
+                    self.server_socket.sendto(b"not Ok", client_address)
+                    func._elogs(f"Пользователь ip {client_address} уже зарегистрирован", ecode = 200)
+                    self.db.delete_all_hosts()
 
-        else:
-            server_socket.sendto(b"not Ok", client_address)
-            func._elogs(f"Пользователь ip {client_address} уже зарегистрирован", ecode = 200)
-            db.delete_all_hosts()
+            elif self.db.get_api_by_ip(client_address[0]) == json.loads(message.decode())["api"]:
+                print("Успешная регистрация")
+                func._elogs(f"Авторизован ip {client_address}", ecode = 200)
+                data = json.loads(message.decode())  
+                print(data)
+                
+                # Добавляем данные в очередь
+                self.data_queue.put(data)
+                
+                continue
 
-    elif db.get_api_by_ip(client_address[0]) == json.loads(message.decode())["api"]:
-        print("Успешная регистрация")
-
-    else:
-        func._elogs(f"Не авторизован ip {client_address}", ecode = 200)
+            else:
+                func._elogs(f"Не авторизован ip {client_address}", ecode = 200)
