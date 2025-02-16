27b23843067cd275be8d38694258e574e5cde98f
Demorein <demorein6@gmail.com>
2025-02-16 23:06:10 +0000

    Запуск 3-х потоков с main.py - выступает хабом для данных
    data_hander - обработчик данных, возвращает данные в виде json в app.py
    
    app.py - web-server

--- mcis/config.py
+++ mcis/config.py
@@ -1,4 +1,4 @@
 
 
-server_host:tuple = "192.168.203.53", 12345
+server_host:tuple = "127.0.0.1", 12345
 database_name:str = "dbdb.db" 
--- mcis/mcis_srv.py
+++ mcis/mcis_srv.py
@@ -1,24 +1,26 @@
 import secrets
 import socket
-import config
-import sql_function
-import func
 import json
 import queue
+import config
+import func
+import sql_function
 
 class mcis_srv:
     def __init__(self, data_queue):
-        self.db = sql_function.sql_func(config.database_name)
+        self.db = sql_function.sql_func(config.database_name)  # Здесь используется sql_function
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.server_socket.bind((f'{config.server_host[0]}', config.server_host[1]))
         self.data_queue = data_queue  # Очередь для передачи данных в основной поток
 
     def start_mcis_srv(self):
+        func._elogs(f"MCIS Сервер запущен на ip {config.server_host[0]}", ecode = 200)
         while True:
             message, client_address = self.server_socket.recvfrom(1024)
             print(message.decode())
+            func._elogs(f"Пакет данных от {client_address}", ecode = 200)
 
-            if message.decode() == "I Here!":
+            if message.decode() == "I Here!": #Регистрация пользователей
                 if self.db.insert_host(client_address[0], 990, secrets.token_hex(16)):
                     print(self.db.show_all_hosts())
                     self.server_socket.sendto(b"OK", client_address)
@@ -27,10 +29,10 @@ class mcis_srv:
                     self.server_socket.sendto(b"not Ok", client_address)
                     func._elogs(f"Пользователь ip {client_address} уже зарегистрирован", ecode = 200)
 
-            elif self.db.get_api_by_ip(client_address[0]) == json.loads(message.decode())["api"]:
+            elif self.db.get_api_by_ip(client_address[0]) == json.loads(message.decode())["api"]: #Приём данных/Проверка токена
                 print("Успешная регистрация")
                 func._elogs(f"Авторизован ip {client_address}", ecode = 200)
-                data = json.loads(message.decode())  
+                data = json.loads(message.decode())
                 print(data)
                 
                 # Добавляем данные в очередь
@@ -40,3 +42,5 @@ class mcis_srv:
 
             else:
                 func._elogs(f"Не авторизован ip {client_address}", ecode = 200)
+
+func._elogs(f"Сервер неожиданно завершил работу", ecode = 500, v="error")
