27e7d6192955de2a8156faf8f69e760339193a1a
Demorein <demorein6@gmail.com>
2025-02-17 01:20:47 +0000

    debug data

--- mcis/test_cli.py
+++ mcis/test_cli.py
@@ -6,8 +6,9 @@ import json
 a = {"data":"datata","api":"20defd967db78f503464f5e8bc5ebb61"}
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client_socket.sendto(json.dumps(a).encode(), ('127.0.0.1', 12345))
-#client_socket.sendto("I Here!".encode(), ('127.0.0.1', 12345))
+
+#client_socket.sendto(json.dumps(a).encode(), ('127.0.0.1', 12345))
+client_socket.sendto("I Here!".encode(), ('127.0.0.1', 12345))
 print("I Here!")
 
 response, _ = client_socket.recvfrom(1024)
