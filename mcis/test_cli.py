4ba5b4263cc3abfa17fd392e7a702498376b5a62
Demorein <demorein6@gmail.com>
2025-02-17 09:34:53 +0000

    get_data packet crash

--- mcis/test_cli.py
+++ mcis/test_cli.py
@@ -3,12 +3,12 @@
 import socket
 import json
 
-a = {"data":"datata","api":"20defd967db78f503464f5e8bc5ebb61"}
+a = {"data":"datata","api":"ed5b041f2efdc50925e062f05565104e"}
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-#client_socket.sendto(json.dumps(a).encode(), ('127.0.0.1', 12345))
-client_socket.sendto("I Here!".encode(), ('127.0.0.1', 12345))
+client_socket.sendto(json.dumps(a).encode(), ('127.0.0.1', 12345))
+#client_socket.sendto("I Here!".encode(), ('127.0.0.1', 12345))
 print("I Here!")
 
 response, _ = client_socket.recvfrom(1024)
